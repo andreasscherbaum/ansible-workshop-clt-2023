fc0803aac82f5825b80733395cd6f12348965661
Andreas Scherbaum <andreas@scherbaum.la>
2023-03-12 10:53:20 +0100

    Set defaults for variables

--- uebungen/templates/index.php
+++ uebungen/templates/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Ansible Workshop f&uuml;r die {{ event }} {{ year }}</title>
+    <title>Ansible Workshop f&uuml;r die {{ event | default("Chemnitzer Linux-Tage") }} {{ year | default("2023") }}</title>
     <meta charset="utf-8">
 </head>
 
 <body>
-    <h1>Ansible Workshop f&uuml;r die {{ event }} {{ year }}</h1>
+    <h1>Ansible Workshop f&uuml;r die {{ event | default("Chemnitzer Linux-Tage") }} {{ year | default("2023") }}</h1>
     <p>
         <ul>
             <li><a href="database.php">Datenbank Test</a></li>
@@ -15,9 +15,9 @@
             <li><a href="jinja2-example.php">Beispiel f&uuml;r Jinja2 Templates</a></li>
         </ul>
     </p>
-    Finden statt in: {{ location }}
+    Finden statt in: {{ location | default("") }}
     <p>
-        &Uuml;bung {{ uebungnr }}
+        &Uuml;bung {{ uebungnr | default("") }}
     </p>
     <p>
         Workshop: <a href="https://chemnitzer.linux-tage.de/{{ workshop_year }}/de/programm/beitrag/{{ workshop_id }}">{{ workshop_name }}</a>
